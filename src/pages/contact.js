52f3ae2698270caaa458159742e7e9073c84ce38
Alberto Fortes Sanchez <albertofortes@MacBook-Pro-de-Alberto.local>
2020-02-04 09:43:52 +0100

    Adding input hidden within form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+//import Img from "gatsby-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { useFormik } from 'formik';
@@ -67,9 +67,9 @@ const formik = useFormik({
       // axios POST
       const appURL = '/contact/'
       axios.post(appURL, {
-          name: values.your_name,
-          email: values.your_email,
-          message: values.your_message
+        name: values.your_name,
+        email: values.your_email,
+        message: values.your_message
       })
       .then(function (response) {
           console.log(response);
@@ -96,6 +96,7 @@ return (
         <h3 className="article__claim t-c">Do you have any project I can help you as freelance? Do you have a cool project and you need to hire me as long-time contractor into the company staff? I've been more than 14 years coding as JavaScript, CSS, HTML… Working with several companies and startups so maybe I can help you.</h3>
         
         <form name="contact" action="/thanks" method="post" className="genericforms" onSubmit={formik.handleSubmit} data-netlify="true" netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="contact" />
           { ( (formik.touched.your_name && formik.errors.your_name) || (formik.touched.your_email && formik.errors.your_email) || (formik.touched.your_message && formik.errors.your_message) )  
             ? <div className="genericforms__alert">
                 { formik.errors.your_email ? (<p>{formik.errors.your_email}</p>) : null }
