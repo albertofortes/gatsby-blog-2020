a70f27a32880c8daba3d54d86b473e082bebc485
Alberto Fortes Sanchez <albertofortes@MacBook-Pro-de-Alberto.local>
2020-02-04 16:43:00 +0100

    Form withput formik

---
+++ src/pages/thanks.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+//import Img from "gatsby-image"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Thanks = ({ children }) => {
+  const data = useStaticQuery(graphql`
+     {
+      file(relativePath: { eq: "images/albertofortes-web-developer.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      site {
+        siteMetadata {
+          title
+          role
+          description
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+    <SEO title="Contact me" description={data.site.siteMetadata.description} />
+    <article className="article">
+      <h2 className="article__title article__title--remark t-c">Thank you!</h2>
+      <div className="article__cont">
+        <h3 className="article__claim t-c">I will read your message ASAP and for sure that I will answer you!.</h3>
+      </div>
+      </article>
+    </Layout>
+  )
+}
+
+export default Thanks
